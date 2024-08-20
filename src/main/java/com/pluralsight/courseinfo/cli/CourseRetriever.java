209069744b56d0f174d379d5c5a3a2e38b3fab3e
marcin.luczak <marcin.luczak@pricefx.com>
2024-08-20 22:42:51 +0200

    Add SLF4J logging to CourseRetriever
    Replaced System.out prints with SLF4J logging in CourseRetriever for better log management. Updated pom.xml to include SLF4J dependencies.

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,21 +1,28 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        System.out.println("CourseRetriever started!");
+
+        LOG.info("CourseRetriever starting");
         if (args.length == 0) {
-            System.out.println("Please provide an author name as first argument");
+            LOG.warn("Please provide an author name as first argument");
             return;
         }
 
         try {
             retrieveCourses(args[0]);
         } catch (Exception e) {
-            System.out.println("Unexpected error: " + e.getMessage());
+            LOG.error("Unexpected error", e);
         }
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for author: " + authorId);
+        LOG.info("Retrieving courses for author '{}'", authorId);
     }
 }
