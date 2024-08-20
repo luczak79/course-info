35fe8b51be885eaf88d033f836268c33aa881535
marcin.luczak <marcin.luczak@pricefx.com>
2024-08-20 22:16:28 +0200

    Add CourseRetriever class and update Maven configuration
    Introduce the `CourseRetriever` class to retrieve courses by author ID from the command line. Additionally, update the Maven configuration to use `maven-compiler-plugin` version 3.10.1 and set the compiler release to 17.

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,21 @@
+package com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+    public static void main(String[] args) {
+        System.out.println("CourseRetriever started!");
+        if (args.length == 0) {
+            System.out.println("Please provide an author name as first argument");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            System.out.println("Unexpected error: " + e.getMessage());
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        System.out.println("Retrieving courses for author: " + authorId);
+    }
+}
