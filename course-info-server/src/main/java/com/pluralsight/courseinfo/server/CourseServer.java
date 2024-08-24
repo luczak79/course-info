dcda25028173bf2a80246821bbe39bddf1324e8a
marcin.luczak <marcin.luczak@pricefx.com>
2024-08-24 22:02:24 +0200

    Add server properties file and dynamic DB filename loading
    Introduced a `server.properties` file to configure database filenames dynamically. Updated `CourseServer` to read the database filename from the properties file, providing better flexibility for database configuration management.

--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
 import java.util.logging.LogManager;
 
 public class CourseServer {
@@ -21,11 +24,22 @@ public class CourseServer {
     private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args) {
-        LOG.info("Starting HTTP server.");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFileName = loadDatabaseFilename();
+        LOG.info("Starting HTTP server with database {}.", databaseFileName);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFileName);
         ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
 
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
+
+    private static String loadDatabaseFilename() {
+        try (InputStream propertiesStream = CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("course-info.database");
+        } catch (IOException ex) {
+            throw new IllegalStateException("Failed to load database filename.", ex);
+        }
     }
 
 }
